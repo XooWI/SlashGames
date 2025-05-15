d79354a562d68e37dd546bbb2e2d64b638e0efd0
Alexandr <knuckles05@yandex.ru>
2025-05-15 11:46:37 +0300

    randomiser

--- slotwindow.cpp
+++ slotwindow.cpp
@@ -1,5 +1,6 @@
 #include "slotwindow.h"
 #include "ui_slotwindow.h"
+#include <QRandomGenerator>
 
 SlotWindow::SlotWindow(QWidget *parent) :
     QWidget(parent),
@@ -15,6 +16,17 @@ SlotWindow::~SlotWindow()
 
 void SlotWindow::on_pushButton_clicked()
 {
-    ui->label->setText("NIGGGGGGGERS");
+    QString val1 = "background-image: url(:/resources/shrimpThvg200200.png);";
+    QString val2 = "background-image: url(:/resources/shrimpThvg200200hover.png);";
+    QString val;
+    int chance = qrand() % ((1 + 1) - 0) + 0;
+    if (chance == 0){
+        val = val1;
+    }else{
+        val = val2;
+    }
+    ui->label->setStyleSheet(val);
+    ui->label_2->setStyleSheet(val);
+    ui->label_3->setStyleSheet(val);
 }
 
