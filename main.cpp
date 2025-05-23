3b67f0621e2eb03f0d6517113e415ea83687335e
Denis <dennis.denigg@gmail.com>
2025-05-23 16:38:55 +0300

    Сorrected errors
    - Add class Roulette in CMakeLists
    - Edit main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
-    Roulette r;
+    RouletteWidget r;
     r.show();
     return a.exec();
 }
